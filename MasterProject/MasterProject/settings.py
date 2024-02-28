5e6decc338461e5a88cefa8d6b543ae6bcd091ad
Akshat Dubey <iakshatdubey@gmail.com>
2024-02-29 01:46:00 +0530

    Add Nginx configurations
    - Added Dockerfile and default.conf files for nginx
    - Introduced nginx service in docker-compose.yml
    - Set `ALLOWED_HOSTS` in settings
    - Added specific Nginx configurations in Django settings
    
    Please note:
    The `ALLOWED_HOSTS` setting can take an environment variable value `ALLOWED_HOSTS_DJANGO`.

--- MasterProject/MasterProject/settings.py
+++ MasterProject/MasterProject/settings.py
@@ -30,7 +30,16 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS_DJANGO', 
+    'django,localhost,127.0.0.1,0.0.0.0,[::1]'
+).split(',')
+
+
+# Configurations for nginx implementation
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = ('localhost')
 
 
 # Application definition
