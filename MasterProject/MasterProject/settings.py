a41dce8b0488f3140a78bec9cc735561647709a8
Akshat Dubey <iakshatdubey@gmail.com>
2024-02-29 02:26:01 +0530

    Remove whitenoise middleware
    Static files are now served directly by Nginx, eliminating the need for whitenoise middleware.

--- MasterProject/MasterProject/settings.py
+++ MasterProject/MasterProject/settings.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,8 +135,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
